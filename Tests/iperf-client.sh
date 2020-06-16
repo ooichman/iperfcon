b71603cfd0daf1c7694788c229f46df62b4ddd69
Oren Oichman <ooichman@redhat.com>
2020-06-17 00:15:53 +0300

    modifing the iperf-client to GOLANG

--- Tests/iperf-client.sh
+++ Tests/iperf-client.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-curl -X GET http://localhost:8080/iperf/status?server=localhost,port=5001,type=json,critical=30000m,warnging=50000m
+curl -X GET http://localhost:8080/iperf/status?server=localhost,port=5001,type=json,critical=3,warnging=5,format=g
+#curl -X POST http://localhost:8080/iperf/status?server=localhost,port=5001,type=json,critical=30000,warnging=50000,format=M
--- iperf-client/iperf-client.go
+++ iperf-client/iperf-client.go
@@ -9,7 +9,28 @@ import (
 	"net/http"
 	"strings"
 	"regexp"
+	"encoding/json"
 )
+
+type IperfOutput struct {
+	End struct {
+		SumSent struct {
+//			Start         int     `json:"start"`
+//			End           float64 `json:"end"`
+//			Seconds       float64 `json:"seconds"`
+//			Bytes         int64   `json:"bytes"`
+			BitsPerSecond float64 `json:"bits_per_second"`
+//			Retransmits   int     `json:"retransmits"`
+		} `json:"sum_sent"`
+		SumReceived struct {
+//			Start         int     `json:"start"`
+//			End           float64 `json:"end"`
+//			Seconds       float64 `json:"seconds"`
+//			Bytes         int64   `json:"bytes"`
+			BitsPerSecond float64 `json:"bits_per_second"`
+		} `json:"sum_received"`
+	} `json:"end"`
+}
 
 type iperfValues struct {
 		serverAddr string
@@ -17,6 +38,7 @@ type iperfValues struct {
 		outputType string
 		iperfCritical string
 		iperfWarning string
+		iperfFormat string
 }
 
 func getEnv(key, fallback string) string {
@@ -27,24 +49,83 @@ func getEnv(key, fallback string) string {
 	return value
 }
 
+func checkFormat(ipc iperfValues) int64 {
+
+	var duplicateNum int64
+	duplicateNum = 1
+	if ipc.iperfFormat == "k" || ipc.iperfFormat == "K" {
+		duplicateNum = duplicateNum * 1024
+	} else if ipc.iperfFormat == "m" || ipc.iperfFormat == "M" {
+		duplicateNum = duplicateNum * 1024 * 1024
+	} else if ipc.iperfFormat == "g" || ipc.iperfFormat == "G" {
+		duplicateNum = duplicateNum * 1024 * 1024 * 1024
+	} else if ipc.iperfFormat == "t" || ipc.iperfFormat == "T" {
+		duplicateNum = duplicateNum * 1024 * 1024 * 1024 * 1024
+	} else {
+		duplicateNum = 1
+	}
+
+	return duplicateNum
+}
+
 func RunningIperf(ipc iperfValues,w http.ResponseWriter) {
 
-
 	iperfCommand := "/usr/bin/iperf3"
-	log.Printf("the comman is %s\n", iperfCommand)
+//	log.Printf("the comman is %s\n", iperfCommand)
 
-	cmd := exec.Command(iperfCommand,"-c",ipc.serverAddr,"-p",ipc.portNumber,"-f","M","-t","2","-i","0.5")
-	out, err := cmd.Output()
+	cmd := exec.Command(iperfCommand,"-c",ipc.serverAddr,"-p",ipc.portNumber,"-J","-t","2","-i","0.5")
+	out, cmderr := cmd.Output()
+
+	if cmderr != nil {
+		fmt.Fprintf(w,"there was an error running iperf with the given arguments: %+v\n",cmderr)
+		http.Error(w,"", 502)
+		return
+	}
+
+	var statusFlag string
+	var iperfSumSent float64
+	var iperfSumRec float64
+	var critalNum float64
+	var warningNum float64
+	var ipout IperfOutput
+	err := json.Unmarshal([]byte(out), &ipout)
 
 	if err != nil {
-        fmt.Fprintf(w,"return Error code from command %s", err)
-    }
+		fmt.Fprintf(w,"there was an error parsing the output: %+v\n",err)
+		http.Error(w,"", 502)
+		return
+	}
+	duplicateSum := checkFormat(ipc)
 
-//    fmt.Fprintf(w, "%s\n", out)
-//	log.Printf("%s\n", out)
+	if duplicateSum != 1 {
 
+	iperfSumSent = ipout.End.SumSent.BitsPerSecond / float64(duplicateSum)
+	iperfSumRec = ipout.End.SumReceived.BitsPerSecond / float64(duplicateSum)
+	
+	} else {
+		fmt.Fprintf(w,"Error: wrong Format type provided\n")
+		http.Error(w,"", 504)
+		return
+	}
+//	if checkFormat(ipc.iperfFormat)
+	sumAvrg := (iperfSumSent + iperfSumRec) / float64(2)
 
-	fmt.Printf("the data is : %s", out)
+	fmt.Sscan(ipc.iperfCritical, &critalNum)
+	fmt.Sscan(ipc.iperfWarning, &warningNum)
+
+//	critalNum = critalNum * float64(duplicateSum)
+//	warningNum = warningNum * float64(duplicateSum)
+	
+	if sumAvrg <= critalNum {
+		statusFlag = "Critical"
+	} else if sumAvrg <= warningNum {
+		statusFlag = "Warning"
+	} else {
+		statusFlag = "O.k"
+	}
+//	fmt.Fprintf(w,"critalNum is : %f , warningNum is %f", critalNum, warningNum)
+	fmt.Fprintf(w,"{ \"Status\": \"%s\" , \"PacketSent\": \"%f\" , \"PacketReceived: \"%f\"\n", statusFlag, iperfSumSent , iperfSumRec )
+
 }
 
 func GetHandle(w http.ResponseWriter, r *http.Request) {
@@ -52,9 +133,10 @@ func GetHandle(w http.ResponseWriter, r *http.Request) {
 	var iperfClient iperfValues
 
 	if r.Method != "GET" {
-		fmt.Fprintf(w, "Only GET Method is allowed\n")
+//		fmt.Fprintf(w, "Only GET Method is allowed\n")
 		log.Printf("Received request Method %s not allowed, GET Only", r.Method)
-		os.Exit(2)
+		http.Error(w,"Only GET Method is allowed\n", 500)
+		return
 	}
 	fullquery := r.URL.RawQuery
 	fullquery = strings.ReplaceAll(fullquery,",","&")
@@ -62,10 +144,10 @@ func GetHandle(w http.ResponseWriter, r *http.Request) {
   
     splitquery := strings.Split(fullquery, "&")
     
-    if len(splitquery) < 5 {
-		fmt.Fprintf(w, "Make sure your Query has all the values %s\n", fullquery)
-		log.Printf("Make sure your Query has all the values %s\n", fullquery)
-    }
+//    if len(splitquery) < 5 {
+//		fmt.Fprintf(w, "Make sure your Query has all the values %s\n", fullquery)
+//		log.Printf("Make sure your Query has all the values %s\n", fullquery)
+//    }
 
     for i := 0 ; i < len(splitquery); i++ {
 //    	fmt.Fprintf(w, "%s\n" , splitquery[i])
@@ -75,33 +157,39 @@ func GetHandle(w http.ResponseWriter, r *http.Request) {
 		if serverReg.MatchString(splitquery[i]) {
 				serverSplit := strings.Split(splitquery[i], "=")
 				iperfClient.serverAddr = serverSplit[1]
-				log.Printf("the Iperf Server IP Address is : %s", iperfClient.serverAddr)
+//				log.Printf("the Iperf Server IP Address is : %s", iperfClient.serverAddr)
 		}
 
 		portReg := regexp.MustCompile(`(port){1}`)
 		if portReg.MatchString(splitquery[i]) {
 				portSplit := strings.Split(splitquery[i], "=")
 				iperfClient.portNumber = portSplit[1]
-				log.Printf("the Iperf Port Number is %s", iperfClient.portNumber)
+//				log.Printf("the Iperf Port Number is %s", iperfClient.portNumber)
 		}
 
 		typeReg := regexp.MustCompile(`(type){1}`)
 		if typeReg.MatchString(splitquery[i]) {
 				typeSplit := strings.Split(splitquery[i], "=")
 				iperfClient.outputType = typeSplit[1]
-				log.Printf("the Iperf output type is %s", iperfClient.outputType)
+//				log.Printf("the Iperf output type is %s", iperfClient.outputType)
 		}
 		warngingReg := regexp.MustCompile(`(warnging){1}`)
 		if warngingReg.MatchString(splitquery[i]) {
 				warngSplit := strings.Split(splitquery[i], "=")
 				iperfClient.iperfWarning = warngSplit[1]
-				log.Printf("the warnging limit is %s", iperfClient.iperfWarning)
+//				log.Printf("the warnging limit is %s", iperfClient.iperfWarning)
 		}
 		criticalReg := regexp.MustCompile(`(critical){1}`)
 		if criticalReg.MatchString(splitquery[i]) {
 				crticalSplit := strings.Split(splitquery[i], "=")
 				iperfClient.iperfCritical = crticalSplit[1]
-				log.Printf("the critical limit is %s", iperfClient.iperfCritical)
+//				log.Printf("the critical limit is %s", iperfClient.iperfCritical)
+		}
+		formatReg := regexp.MustCompile(`(format){1}`)
+		if formatReg.MatchString(splitquery[i]) {
+				formatSplit := strings.Split(splitquery[i], "=")
+				iperfClient.iperfFormat = formatSplit[1]
+//				log.Printf("the Format is %s", iperfClient.iperfFormat)
 		}
     }
 
